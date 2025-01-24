37ba6759d48aa3ed676214d8e89066ca1fbdbc08
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2025-01-25 01:05:59 +0500

    Review dynamiced + UI updated

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -1,47 +1,67 @@
-"use client";
+import { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "./../ui/infinite-moving-cards.tsx";
-
-const testimonials = [
-  {
-    quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
-  },
-  {
-    quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet",
-  },
-  {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
-  },
-  {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
-  },
-  {
-    quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
-  },
-];
+import axios from "axios";
 
 export function Reviews() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const reviewsResponse = await axios.get("/review");
+
+        if (reviewsResponse.data && reviewsResponse.data.length > 0) {
+          const complete = await Promise.all(
+            reviewsResponse.data.map(async (review) => {
+              const customerResponse = await axios.get(
+                `/customers/${review.customerId}`
+              );
+              const customerName = customerResponse.data.name;
+              const productResponse = await axios.get(
+                `/products/${review.productId}`
+              );
+              const productName = productResponse.data.name;
+              return {
+                ...review,
+                customerName,
+                productName,
+              };
+            })
+          );
+
+          setReviews(complete);
+        } else {
+          console.log("No reviews found");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <h1 className="text-4xl font-bold dark:text-white">Testimonials</h1>
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
+    <div className='h-[40rem] rounded-md flex flex-col gap-10 antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden'>
+      <h1 className='text-4xl font-bold dark:text-white'>Testimonials</h1>
+      {reviews.length > 0 ? (
+        <InfiniteMovingCards
+          items={reviews.map((review) => ({
+            name: review.customerName,
+            quote: review.review,
+            title: review.productName,
+          }))}
+          direction='right'
+          speed='slow'
+        />
+      ) : (
+        <div className='text-center text-2xl font-medium text-gray-500 col-span-full'>
+          No reviews found
+        </div>
+      )}
     </div>
   );
 }
+
+/* Made by Labeeb Tariq */
+/* Dynamic by: Wali M. Ahmad */
